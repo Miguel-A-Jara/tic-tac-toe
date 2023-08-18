34b3621dfabfad5d47a2edc3a46e828a1f5ce716
Miguel Jara <0miguel.jara15@gmail.com>
2023-08-17 22:14:06 -0400

    chore(General): add .env file to set up local port

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '~': '/src',
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: { port: parseInt(env.APP_PORT) },
+    resolve: { alias: { '~': '/src' } },
+  }
 })
